5cb2dad18d0f98fe0150155d49cbd6f0d0cf8bb4
Simon Byrne <simonbyrne@gmail.com>
2017-08-02 11:14:44 -0700

    use testset

--- test.jl
+++ test.jl
@@ -8,6 +8,8 @@ unsafe_store!(cglobal((:norm_rand_ptr,libRmath),Ptr{Void}),
 unsafe_store!(cglobal((:exp_rand_ptr,libRmath),Ptr{Void}),
               cfunction(randexp,Float64,()))
 
-@test ccall((:dbeta,libRmath), Float64, (Float64,Float64,Float64,Int32), 0.5, 0.1, 5.0, 0) ≈ 0.014267678091051986
-@test 0 <= ccall((:rbeta,libRmath), Float64, (Float64,Float64), 0.1, 5.0) <= 1.0
+@testset "ccall" begin
+    @test ccall((:dbeta,libRmath), Float64, (Float64,Float64,Float64,Int32), 0.5, 0.1, 5.0, 0) ≈ 0.014267678091051986
+    @test 0 <= ccall((:rbeta,libRmath), Float64, (Float64,Float64), 0.1, 5.0) <= 1.0
+end
 
